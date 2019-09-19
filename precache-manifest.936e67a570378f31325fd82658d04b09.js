d3f9816b7b5073404e632ddb41c0d5015f3be201
Ryan Luu <ryan@vumble.com>
2019-09-18 20:59:02 -0400

    Updates

---
+++ precache-manifest.936e67a570378f31325fd82658d04b09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f8a8b629121e55c7d646d044c4a22e",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "f8f93eb170f56dd6816f",
+    "url": "/landing/static/css/main.72eeda64.chunk.css"
+  },
+  {
+    "revision": "642903ef0b11663d3820",
+    "url": "/landing/static/js/2.92fc5c93.chunk.js"
+  },
+  {
+    "revision": "f8f93eb170f56dd6816f",
+    "url": "/landing/static/js/main.eefff1c4.chunk.js"
+  },
+  {
+    "revision": "5f2968b253030ef2ed45",
+    "url": "/landing/static/js/runtime~main.97251619.js"
+  },
+  {
+    "revision": "e1847c08c3bf7963578e3976644aaebf",
+    "url": "/landing/static/media/discord.e1847c08.svg"
+  },
+  {
+    "revision": "12fb2c96368030aa08429972568eb8e5",
+    "url": "/landing/static/media/hero-background.12fb2c96.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/landing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9211246223b818c15856e3fc476bc88c",
+    "url": "/landing/static/media/oculus.92112462.svg"
+  }
+]);
